639d274233aa5651139cd5ee4ae0c3244619ef2f
Adam Rutkowski <a_rutkowski@outlook.com>
2016-09-01 23:17:44 +0200

    Pause button extracted to separate control, can be disabled.

---
+++ src/VIDEOwnloader/Controls/PauseButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace VIDEOwnloader.Controls
+{
+    public class PauseButton : ToggleButton
+    {
+        static PauseButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PauseButton),
+                new FrameworkPropertyMetadata(typeof(PauseButton)));
+        }
+    }
+}
